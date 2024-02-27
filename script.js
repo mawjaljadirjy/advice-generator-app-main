e180d4c54d350a70f947d664d21248bde1dbef42
Mawj_Aljadirjy <eng_mawj_aljadirjy@yahoo.com>
2024-02-27 22:26:32 +0300

    add files

---
+++ script.js
@@ -0,0 +1,29 @@
+const adviceTitle = document.querySelector(".advice-title");
+const adviceText = document.querySelector(".advice-text");
+const getDataBtn = document.querySelector(".get-data"); 
+
+
+window.addEventListener("load", getData);
+
+getDataBtn.addEventListener("click", function(){
+    getData();
+})
+
+
+function getData(){
+    const randomId = Math.floor(Math.random() * 1000000);
+    const apiUrl = `https://api.adviceslip.com/advice?random=${randomId}`;
+
+    fetch(apiUrl).then(response => {
+        
+    return response.json();
+}).then(adviceData => {
+
+    const adviceObject = adviceData.slip;
+
+    adviceText.innerHTML = `"${adviceObject.advice}"`;
+    adviceTitle.innerHTML = `Advice #${adviceObject.id}`;
+}).catch(error => {
+    console.log(error);
+});
+}
